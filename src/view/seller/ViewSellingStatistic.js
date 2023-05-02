3b6ce87139c323dea09c39c17b7608b7a1fac6c7
Vuxz123 <ethnicthv123@gmail.com>
2023-05-02 17:58:17 +0700

    pre-release 2.0

--- src/view/seller/ViewSellingStatistic.js
+++ src/view/seller/ViewSellingStatistic.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Box, Text, Flex, Stat, StatLabel, StatNumber, StatHelpText } from "@chakra-ui/react";
 import MyNavbar from "../../component/nav/MyNavbar";
+import server from "../../util/restful/Server";
+import {useAuth0} from "@auth0/auth0-react";
 
 function SellingStatisticView() {
+    const [seller, setSeller] = useState(null);
+
+    const {user} = useAuth0();
+
+    useEffect(() => {
+        async function fetchData() {
+            const res = await server.get("/seller/" + user.sub);
+            console.log(res);
+            setSeller(res.data);
+        }
+
+        fetchData();
+    }, []);
+
+    const total = seller;
+
     return (
         <div>
             <div>
